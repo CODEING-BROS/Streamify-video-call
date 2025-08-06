efe2bffd2ccf953d6d8b4a314c42aeef36b85145
codewithstella <coderluffy35@gmail.com>
2025-08-06 08:43:39 +0530

    first commit

--- BACKEND/src/server.js
+++ BACKEND/src/server.js
@@ -29,7 +29,7 @@ app.use('/api/users', userRoutes);
 app.use('/api/chat', chatRoutes);
 
 if (process.env.NODE_ENV === 'production') {
-  const frontendPath = path.resolve(__dirname, '../../frontend/dist');
+  const frontendPath = path.resolve(__dirname, '../frontend/dist');
   app.use(express.static(frontendPath));
   app.get('*', (req, res) => {
     res.sendFile(path.join(frontendPath, 'index.html'));
